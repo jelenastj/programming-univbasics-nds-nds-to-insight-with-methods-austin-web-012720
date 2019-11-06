bab1e54a766998007ab72f08278047405d75fe62
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-06 17:20:58 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -40,10 +40,8 @@ def gross_for_director(director_data)
   while index < director_data[:movies].length do
     total+=director_data[:movies][index][:worldwide_gross]
     index+=1 
-    
-  
-  
-  
+  end
+  total
 end
 
 pp directors_database
