b28a89db202574375887a85935e1617668166dd6
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-06 17:00:44 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,5 +12,8 @@ end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
+
 def gross_for_director(director_data)
 end
+
+pp directors_database
