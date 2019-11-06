67cffbffdc9624868ffc1e63211f63b0f9597cd7
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-06 17:43:44 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,16 +13,8 @@ def directors_totals(nds)
   index = 0 
   while index< nds.length do
     
-     sum = 0 
-     movie_number = 0 
+    result[nds[index][:name]]=gross_for_director(director_data)
     
-    while movie_number <nds[index][:movies].length do
-     
-        sum+= nds[index][:movies][movie_number][:worldwide_gross]
-        movie_number +=1 
-      end
-      result[nds[index][:name]]= sum 
-      
       index += 1 
       end
  result
